7042a0cd84f113c3b4f35c8d57c2bb627f26d309
Josep Llaurado <pep@beyondbluesky.com>
2026-03-29 23:09:19 +0200

    Release 1.4.1 bugfix

--- src/OAuth2PKCEClientBundle.php
+++ src/OAuth2PKCEClientBundle.php
@@ -12,16 +12,15 @@
 namespace BeyondBlueSky\OAuth2PKCEClient;
 
 use BeyondBlueSky\OAuth2PKCEClient\DependencyInjection\OAuth2PKCEClientExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OAuth2PKCEClientBundle extends Bundle
 {
     /**
      * Overridden to allow for the custom extension alias.
-     *
-     * @return KnpUOAuth2ClientExtension
      */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             return new OAuth2PKCEClientExtension();
